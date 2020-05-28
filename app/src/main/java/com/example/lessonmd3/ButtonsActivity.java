fe718e14d19130eac7ee2156f37eb1d45a6add42
Lukoil39 <goshavasilevskiy@mail.ru>
2020-05-28 20:59:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lessonmd3/ButtonsActivity.java
@@ -0,0 +1,14 @@
+package com.example.lessonmd3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ButtonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+}
